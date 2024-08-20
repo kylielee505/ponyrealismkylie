9c3a2b7ea8e6b1f02ff72fb5510d42ac790009c7
gokay aydogan <gokaygokay@users.noreply.huggingface.co>
2024-08-20 22:09:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ with gr.Blocks(theme='bethecloud/storj_theme') as demo:
     with gr.Row():
         with gr.Column(scale=1):
             model_choice = gr.Dropdown(
-                    ["Pony Realism v21", "Cyber Realistic Pony v61", "Stallion Dreams Pony Realistic v1"], 
+                    ["Pony Realism v21", "Cyber Realistic Pony v62", "Stallion Dreams Pony Realistic v1"], 
                     label="Model Choice", 
                     value="Pony Realism v21")
             positive_prompt = gr.Textbox(label="Positive Prompt", placeholder="Add your positive prompt here")
